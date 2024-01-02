8a0f67d6794f3e5d6079b005b400a65ac873b0fa
Shaw Joseph (R0A) MFT <Joseph.Shaw2@mft.nhs.uk>
2024-01-02 16:51:34 +0000

    Update extract_cnv_calls

--- functions/cnv_functions.R
+++ functions/cnv_functions.R
@@ -39,46 +39,33 @@ save_plot <- function(input_plot, input_width = 15, input_height = 12, dpi = 300
 
 # Data wrangling functions ----------------------------------------------------------
 
-extract_cnv_calls <- function(df) {
+extract_cnv_calls <- function(df, input_gene) {
   
   stopifnot("Genotype" %in% colnames(df))
   
-  dq_regex <- regex(
-        r"[
-        (E.{2}B2|EGFR|MYC|MET|ARID1A|SUFU|)\s  # Gene names
-        (amplification\sdetected|amplification)
-        \s.                                 # Use . for bracket
-        (Mean\sDQ|mean\sDQ|DQ)
-        \s
-        (\d{1,3}|\d{1,3}\.\d{2})            # Dosage quotient
-        x
-        ]",
-        comments = TRUE
-      )
+  dq_regex <- regex(str_c(
+    # Group - input gene
+    "(",input_gene,")\\s",
+    # Group - variable freetype,
+    "(amplification\\sdetected|amplification)",
+    # Use . for bracket
+    "\\s.",
+    # Group - variable freetype
+    "(Mean\\sDQ|mean\\sDQ|DQ)",
+    "\\s",
+    # Group - dosage quotient regex
+    "(\\d{1,3}|\\d{1,3}\\.\\d{2})",
+    "x"))
 
-  cnv_calls <- unique(grep(pattern = "DQ", x = df$Genotype, ignore.case = TRUE,
-                           value = TRUE))
-  
-  no_cnv_calls <- unique(grep(pattern = "No evidence for a clinically relevant CNV", 
-                              x = df$Genotype, ignore.case = TRUE,
-                              value = TRUE))
-  
-  no_erbb2_calls <- unique(grep(pattern = "No ERBB2 amplification detected", 
-                                x = df$Genotype, ignore.case = TRUE,
-                                value = TRUE))
-  
   output <- df |> 
-    mutate(
-      core_cnv_status = case_when(
-        Genotype %in% cnv_calls ~"CNVs detected",
-        Genotype %in% no_cnv_calls | Genotype %in% no_erbb2_calls ~"No CNVs detected",
-        TRUE ~"Not specified"
-      ),
-      target_gene = str_extract(Genotype, dq_regex, group = 1), 
-      target_gene = ifelse(target_gene == "ERRB2", "ERBB2", target_gene),
-      target_gene_dq = as.numeric(str_extract(Genotype, dq_regex, group = 4)))
+    select(LABNO, Genotype) |> 
+    mutate(gene_searched = input_gene,
+           gene_match = str_extract(Genotype, dq_regex, group = 1),
+           gene_dq = as.numeric(str_extract(Genotype, dq_regex, group = 4)),
+           core_result = ifelse(!is.na(gene_dq), "Amplification", "No call"))
   
   return(output)
+  
 }
 
 get_excel_names <- function(filepath, folder) {
@@ -129,6 +116,9 @@ summarise_results <- function(file, input_sheet) {
   results <- read_excel(path = file,
                         sheet = input_sheet) |> 
     janitor::clean_names()
+  
+  worksheet <- str_extract(file, filename_regex,
+                          group = 1)
   
   sample_id <- str_extract(file, filename_regex,
                            group = 2)
