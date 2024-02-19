1aba3ca607bbe8d24dbb498c07b715cd8415dfce
Shaw Joseph (R0A) MFT <Joseph.Shaw2@mft.nhs.uk>
2024-02-19 13:53:27 +0000

    Change TCC to NCC

--- functions/cnv_functions.R
+++ functions/cnv_functions.R
@@ -622,17 +622,17 @@ draw_confusion_matrix <- function(input_gene) {
   
 }
 
-calculate_target_copies <- function(fold_change, tcc_percent) {
+calculate_target_copies <- function(fold_change, ncc_percent) {
   
   ref_sample_copies <- 200
   
-  tcc_fraction <- tcc_percent / 100
+  ncc_fraction <- ncc_percent / 100
   
   sample_total_target_copies <- fold_change * ref_sample_copies
   
-  sample_target_copies_in_tumour_cells <- sample_total_target_copies - ((100 * (1-tcc_fraction)) * 2)
+  sample_target_copies_in_tumour_cells <- sample_total_target_copies - ((100 * (1-ncc_fraction)) * 2)
   
-  sample_target_copies_per_tumour_cell <- sample_target_copies_in_tumour_cells / (100 * tcc_fraction)
+  sample_target_copies_per_tumour_cell <- sample_target_copies_in_tumour_cells / (100 * ncc_fraction)
   
   return(sample_target_copies_per_tumour_cell)
   
@@ -692,12 +692,6 @@ make_confusion_matrix <- function(df, input_column = outcome,
   return(list(conf_matrix, opa))
   
 }
-
-
-
-
-
-
 
 # Database tables -------------------------------------------------------------------
 
