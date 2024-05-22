73dff844e5af23824c443deaa268992758d58f1a
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-05-22 16:22:26 +0100

    Copy WGS results to local folder

--- scripts/finding_wgs_samples_with_qiasymphony extractions.R
+++ scripts/finding_wgs_samples_with_qiasymphony extractions.R
@@ -71,10 +71,38 @@ lab_nos_with_method <- all_samples_from_wgs_patients_df |>
   arrange(nhsno) |> 
   left_join(wgs_pathway_tracker_dna_no_df |> 
               select(labno, ngis_patient_id, ngis_referral_id),
-            by = "labno")
+            by = "labno") |> 
+  arrange(nhsno)
 
 qiasymphony_lab_nos <- lab_nos_with_method |> 
   filter(method_name == "QIAsymphony_DNA_FFPE")
 
 wgs_samples_with_qiasymphony_dna <- lab_nos_with_method |> 
-  filter(nhsno %in% qiasymphony_lab_nos$nhsno)
+  filter(nhsno %in% qiasymphony_lab_nos$nhsno &
+           !is.na(ngis_patient_id))
+
+# Get HTML files --------------------------------------------------------------------
+
+patient_ids_to_get <- wgs_samples_with_qiasymphony_dna$ngis_patient_id
+
+clin_trial_filepath <- "S:/central shared/Genetics/Mol_Shared/Cancer Team/100kGP Cancer Program Validation & Feedback/Live Clinical Trials/"
+
+file_regex <- "\\d{10}_p\\d{11}_LP\\d{7}-DNA_\\w{1}\\d{2}_LP\\d{7}-DNA_\\w{1}\\d{2}.+(supplementary|supplementary\\s\\(1\\)|supplementary\\s\\(2\\)).html$"
+
+all_htmls <- list.files(path = str_c(clin_trial_filepath, "MTB sheets/"),
+                        pattern = file_regex,
+                        recursive = TRUE,
+                        full.names = TRUE,
+                        ignore.case = TRUE)
+
+all_html_df <- data.frame(
+  s_drive_filepath = all_htmls) |> 
+  mutate(wgs_p_no = str_extract(string = s_drive_filepath,
+                                pattern = "p\\d{11}"))
+
+file_path_df_samples_to_get <- all_html_df |> 
+  filter(wgs_p_no %in% patient_ids_to_get)
+
+file_paths_to_copy <- file_path_df_samples_to_get$s_drive_filepath
+
+file.copy(from = file_paths_to_copy, to = here::here("data/wgs_result_htmls/"))
