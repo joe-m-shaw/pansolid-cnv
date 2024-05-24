f5bed86183a647bbabe2c700be7760bb2f907a52
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-05-24 15:38:34 +0100

    Update to account for new filename regex

--- scripts/update_pansolid_cnv_service_data.R
+++ scripts/update_pansolid_cnv_service_data.R
@@ -23,9 +23,14 @@ s_drive_filepaths <- worksheet_list |>
 s_drive_file_df <- tibble(
   filepath = unlist(s_drive_filepaths)) |> 
   mutate(filename = str_extract(string = filepath, 
-                                pattern = "Annotated_WS\\d{6}_.+.xlsx"),
+                         pattern = str_replace(string = pansolidv2_excel_regex, 
+                                               pattern = "\\^", 
+                                               replacement = "")),
          labno = str_extract(string = filename, 
-                             pattern = "\\d{8}"))
+                             pattern = "WS\\d{6}_(\\d{6,8})_",
+                             group = 1))
+
+stopifnot(anyNA(s_drive_file_df) == FALSE)
 
 # Local drive filepaths -------------------------------------------------------------
 
@@ -33,9 +38,12 @@ local_drive_file_df <- tibble(
   filepath = unlist(list.files(here::here("data/live_service_annotated_files/"),
                                full.names = TRUE))) |> 
   mutate(filename = str_extract(string = filepath, 
-                                pattern = "Annotated_WS\\d{6}_.+.xlsx"),
+                                pattern = str_replace(string = pansolidv2_excel_regex, 
+                                                      pattern = "\\^", 
+                                                      replacement = "")),
          labno = str_extract(string = filename, 
-                             pattern = "\\d{8}"))
+                             pattern = "WS\\d{6}_(\\d{6,8})_",
+                             group = 1))
 
 # Identify and copy new files -------------------------------------------------------
 
@@ -56,9 +64,12 @@ local_drive_file_df <- tibble(
   filepath = unlist(list.files(here::here("data/live_service_annotated_files/"),
                                full.names = TRUE))) |> 
   mutate(filename = str_extract(string = filepath, 
-                                pattern = "Annotated_WS\\d{6}_.+.xlsx"),
+                                pattern = str_replace(string = pansolidv2_excel_regex, 
+                                                      pattern = "\\^", 
+                                                      replacement = "")),
          labno = str_extract(string = filename, 
-                             pattern = "\\d{8}"))
+                             pattern = "WS\\d{6}_(\\d{6,8})_",
+                             group = 1))
 
 new_file_local_paths_df <- local_drive_file_df |> 
   filter(filename %in% new_files$filename & 
@@ -71,11 +82,23 @@ new_file_local_paths <- list(new_file_local_paths_df$filepath) |>
 # Collate new file data -------------------------------------------------------------
 
 new_amp_gene_collated <- new_file_local_paths |> 
-  map(\(new_file_local_paths) read_annotated_file_all_amp(new_file_local_paths)) |> 
+  map(\(new_file_local_paths) read_all_amp_genes_results(file = new_file_local_paths,
+                                                         sheet = get_amp_sheetname(new_file_local_paths))) |> 
+  list_rbind()
+
+new_pos_cnv_collated <- new_file_local_paths |> 
+  map(\(new_file_local_paths) read_pos_cnv_results(file = new_file_local_paths,
+                                                   sheet = get_amp_sheetname(new_file_local_paths))) |> 
   list_rbind()
 
 new_std_dev_collated <- new_file_local_paths |> 
-  map(\(new_file_local_paths) read_annotated_file_stdev(new_file_local_paths)) |> 
+  map(\(new_file_local_paths) read_stdev_results(file = new_file_local_paths,
+                                                        sheet = get_amp_sheetname(new_file_local_paths))) |> 
+  list_rbind()
+
+new_percent_138_collated <- new_file_local_paths |> 
+  map(\(new_file_local_paths) read_percent_138_results(file = new_file_local_paths,
+                                                        sheet = get_amp_sheetname(new_file_local_paths))) |> 
   list_rbind()
 
 # Load previously collated data -----------------------------------------------------
@@ -84,59 +107,78 @@ amp_gene_results <- read_csv(here::here("data/live_service_collated_data/live_se
 
 std_dev_results <- read_csv(here::here("data/live_service_collated_data/live_service_std_dev_results_collated.csv"))
 
+percent_138_results <- read_csv(here::here("data/live_service_collated_data/live_service_percent_138_results_collated.csv"))
+
+pos_cnv_results <- read_csv(here::here("data/live_service_collated_data/live_service_pos_cnv_results_collated.csv"))
+
 # Check columns ---------------------------------------------------------------------
 
 # Scientists have been adding comments as extra columns, which makes binding the data
 # tricky.
 
-all_amp_cols <- c("worksheet", "labno", "suffix", "patient_name", 
-                 "labno_suffix", "labno_suffix_worksheet", "filepath", 
-                 "gene", "max_region_fold_change", "min_region_fold_change")
+id_cols <- c("worksheet", "labno", "suffix", "patient_name", 
+             "labno_suffix", "labno_suffix_worksheet", "filepath")
 
-std_dev_cols <- c("worksheet", "labno", "suffix", "patient_name", 
-                  "labno_suffix", "labno_suffix_worksheet", "filepath", 
-                  "st_dev_signal_adjusted_log2_ratios")
+all_amp_cols <- c(id_cols, "gene", "max_region_fold_change", "min_region_fold_change")
 
-amp_gene_results_cols_defined <- amp_gene_results |> 
-  select(all_of(all_amp_cols))
+std_dev_cols <- c(id_cols, "st_dev_signal_adjusted_log2_ratios")
 
-std_dev_results_cols_defined <- std_dev_results |> 
-  select(all_of(std_dev_cols))
+pos_cnv_cols <- c(id_cols, "gene",	"chromosome",	"cnv_co_ordinates",	"cnv_length",
+                  "consequence", "fold_change",	"p_value",	
+                  "no_targets",	"start",	"end")
 
-new_amp_gene_collated_cols_defined <- new_amp_gene_collated |> 
-  select(all_of(all_amp_cols))
-
-new_std_dev_collated_cols_defined <- new_std_dev_collated |> 
-  select(all_of(std_dev_cols))
-
-stopifnot(colnames(amp_gene_results_cols_defined) == 
-            colnames(new_amp_gene_collated_cols_defined))
-
-stopifnot(colnames(std_dev_results_cols_defined) == 
-            colnames(new_std_dev_collated_cols_defined))
+percent_138_cols <- c(id_cols, "percent_whole_panel_covered_at_138x")
 
 # Add new data to collated data -----------------------------------------------------
 
-amp_gene_results_updated <- rbind(amp_gene_results_cols_defined,
-                                  new_amp_gene_collated_cols_defined)
+amp_gene_results_updated <- rbind(amp_gene_results |> 
+                                    select(all_of(all_amp_cols)),
+                                  new_amp_gene_collated |> 
+                                    select(all_of(all_amp_cols)))
 
-std_dev_results_updated <- rbind(std_dev_results_cols_defined,
-                                 new_std_dev_collated_cols_defined)
+pos_cnv_results_updated <- rbind(pos_cnv_results |> 
+                                   select(all_of(pos_cnv_cols)),
+                                 new_pos_cnv_collated |> 
+                                   select(all_of(pos_cnv_cols)))
+
+std_dev_results_updated <- rbind(std_dev_results |> 
+                                   select(all_of(std_dev_cols)),
+                                 new_std_dev_collated |> 
+                                   select(all_of(std_dev_cols)))
+
+percent_138_results_updated <- rbind(percent_138_results |> 
+                               select(all_of(percent_138_cols)),
+                             new_percent_138_collated |> 
+                               select(all_of(percent_138_cols)))
 
 # Archive previous collated data ----------------------------------------------------
 
-write.csv(amp_gene_results_cols_defined,
+write.csv(amp_gene_results,
           here::here(str_c("data/live_service_collated_data/archived_collated_data/",
                            format(Sys.time(), "%Y_%m_%d_%H_%M_%S"),
                            "_",
                            "live_service_amp_gene_results_collated.csv")),
           row.names = FALSE)
 
-write.csv(std_dev_results_cols_defined,
+write.csv(std_dev_results,
           here::here(str_c("data/live_service_collated_data/archived_collated_data/",
                            format(Sys.time(), "%Y_%m_%d_%H_%M_%S"),
                            "_",
                            "live_service_std_dev_results_collated.csv")),
+          row.names = FALSE)
+
+write.csv(pos_cnv_results,
+          here::here(str_c("data/live_service_collated_data/archived_collated_data/",
+                           format(Sys.time(), "%Y_%m_%d_%H_%M_%S"),
+                           "_",
+                           "live_service_pos_cnv_results_collated.csv")),
+          row.names = FALSE)
+
+write.csv(percent_138_results,
+          here::here(str_c("data/live_service_collated_data/archived_collated_data/",
+                           format(Sys.time(), "%Y_%m_%d_%H_%M_%S"),
+                           "_",
+                           "live_service_percent_138_results_collated.csv")),
           row.names = FALSE)
 
 # Save updated collated data --------------------------------------------------------
@@ -149,4 +191,14 @@ write.csv(amp_gene_results_updated,
 write.csv(std_dev_results_updated,
           here::here(str_c("data/live_service_collated_data/",
                            "live_service_std_dev_results_collated.csv")),
+          row.names = FALSE)
+
+write.csv(pos_cnv_results_updated,
+          here::here(str_c("data/live_service_collated_data/",
+                           "live_service_pos_cnv_results_collated.csv")),
+          row.names = FALSE)
+
+write.csv(percent_138_results_updated,
+          here::here(str_c("data/live_service_collated_data/",
+                           "live_service_percent_138_results_collated.csv")),
           row.names = FALSE)
