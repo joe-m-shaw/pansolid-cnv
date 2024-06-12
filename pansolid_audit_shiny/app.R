40376c1bebdc9ab6c826ff4114408c40dc17997d
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-06-12 11:02:56 +0100

    Update Shiny app with new CRC referral table

--- pansolid_audit_shiny/app.R
+++ pansolid_audit_shiny/app.R
@@ -5,9 +5,15 @@
 library(shiny)
 library(tidyverse)
 
+source(here::here("functions/cnv_functions.R"))
+
 # Load Data -------------------------------------------------------------------------
 
-amp_gene_results <- read_csv(here::here("data/live_service_collated_data/live_service_amp_gene_results_collated.csv"))
+amp_gene_results <- read_csv(here::here("data/live_service_collated_data/live_service_amp_gene_results_collated.csv")) |> 
+  mutate(filename = str_extract(string = filepath, 
+                         pattern = str_replace(string = pansolidv2_excel_regex, 
+                                               pattern = "\\^", 
+                                               replacement = "")))
 
 std_dev_results <- read_csv(here::here("data/live_service_collated_data/live_service_std_dev_results_collated.csv")) |> 
   rename(noise = st_dev_signal_adjusted_log2_ratios)
@@ -15,7 +21,13 @@ std_dev_results <- read_csv(here::here("data/live_service_collated_data/live_ser
 percent_138_results <- read_csv(here::here("data/live_service_collated_data/live_service_percent_138_results_collated.csv"))
 
 pos_cnv_results <- read_csv(here::here("data/live_service_collated_data/live_service_pos_cnv_results_collated.csv")) |> 
-  mutate(gene = factor(gene, levels = unique(amp_gene_results$gene)))
+  mutate(gene = factor(gene, levels = unique(amp_gene_results$gene)),
+         filename = str_extract(string = filepath, 
+                                pattern = str_replace(string = pansolidv2_excel_regex, 
+                                                      pattern = "\\^", 
+                                                      replacement = "")))
+
+panel_info <- read_csv(here::here("data/live_service_collated_data/pansolidv2_sample_worksheet_panel_information.csv"))
 
 pos_cnv_results_with_qc <- pos_cnv_results |> 
   left_join(std_dev_results |> 
@@ -23,7 +35,25 @@ pos_cnv_results_with_qc <- pos_cnv_results |>
             by = "filepath") |> 
   left_join(percent_138_results |> 
               select(filepath, percent_whole_panel_covered_at_138x),
-            by = "filepath")
+            by = "filepath") |> 
+  left_join(panel_info |> 
+              select(filename, panel),
+            by = "filename",
+            # Some samples are run on multiple panels so have multiple rows
+            relationship = "many-to-many")
+
+fold_change_threshold <- 2.8
+
+crc_erbb2_results <- amp_gene_results |> 
+  left_join(panel_info |> 
+              filter(panel == "v2M1_CRC_PS") |> 
+              select(filename, panel),
+            by = "filename") |> 
+  filter(!is.na(panel) & gene == "ERBB2") |> 
+  mutate(result = case_when(
+    max_region_fold_change >= fold_change_threshold ~"ERBB2 Amplification",
+    max_region_fold_change < fold_change_threshold ~"No ERBB2 amplification"
+  ))
 
 qc_joined <- std_dev_results |> 
   left_join(percent_138_results |> 
@@ -61,27 +91,50 @@ ui <- fluidPage(
   str_c("Samples per week: ", samples_per_week),
 
   fluidRow(
-  column(5,
-         checkboxGroupInput(inputId = "gene_multiple", 
-                            label = "Genes to plot", 
-                            choices = gene_options,
-                            selected = gene_options, inline = TRUE),
-         sliderInput("y_axis_range", "Fold change range", min = -5, 
-                     max = max(pos_cnv_results$fold_change + 5),
-                     value = c(-5, max(pos_cnv_results$fold_change + 5))),
-         plotOutput("gene_amp_plot")),
+    h1("Summary"),
+    
+    column(5,
+           h2("All amplification results"),
+           tableOutput("summary_gene_table")),
+    column(5,
+           h2("Colorectal referrals"),
+           tableOutput("crc_summary_table")),
+  ),
   
-  column(5,
-         selectInput(inputId = "ws_select", label = "Worksheet to highlight",
-                     choices = ws_options, selected = "WS140721"),
-         
-         plotOutput("ws_noise_plot"))
+  fluidRow(
+    h1("Quality Tracking"),
+    
+    column(10,
+           plotOutput("noise_by_ws_plot")),
+    
+    column(10,
+           plotOutput("percent_138_plot"))
+    
+  ),
+  
+  fluidRow(
+    h1("Interactive plots and tables"),
+    
+    column(5,
+           checkboxGroupInput(inputId = "gene_multiple", 
+                              label = "Genes to plot", 
+                              choices = gene_options,
+                              selected = gene_options, inline = TRUE),
+           sliderInput("y_axis_range", "Fold change range", min = -5, 
+                       max = max(pos_cnv_results$fold_change + 5),
+                       value = c(-5, max(pos_cnv_results$fold_change + 5))),
+           plotOutput("gene_amp_plot")),
+    
+    column(5,
+           selectInput(inputId = "ws_select", label = "Worksheet to highlight",
+                       choices = ws_options, selected = "WS140721"),
+           
+           plotOutput("ws_noise_plot"))
+    
+    
   ),
   fluidRow(
-    column(5,
-           h2("Summary table"),
-           tableOutput("summary_gene_table")
-    ),
+    
     column(5,
            h2("Sample details"),
            
@@ -107,16 +160,26 @@ server <- function(input, output) {
       count(gene, .drop = FALSE) |> 
       arrange(desc(n)) |> 
       rename(Cases = n,
-             Result = gene)
+             `Amplification Result` = gene)
     
   })
 
+  output$crc_summary_table <- renderTable({
+    
+    crc_erbb2_results |> 
+      count(result) |> 
+      arrange(desc(n)) |> 
+      rename(Cases = n,
+             `Amplification Result` = result)
+    
+  })
+  
   data_filtered <- reactive({
     
     pos_cnv_results_with_qc |> 
       filter(gene == input$gene_select & 
                noise < input$noise_qc) |> 
-      select(worksheet, labno,  
+      select(worksheet, panel, labno,  
              fold_change, noise)
 
   })
@@ -146,7 +209,6 @@ server <- function(input, output) {
     
   })
   
-  
   output$amp_tbl <- renderTable({
     
     data_filtered() 
@@ -174,6 +236,27 @@ server <- function(input, output) {
       xlim(0, max(qc_joined$noise) + 0.1) +
       geom_point(data = noise_data_filtered(), size = 3,
                  shape = 21, fill = "red")
+    
+  })
+  
+  output$noise_by_ws_plot <- renderPlot({
+    
+    ggplot(std_dev_results, aes(x = worksheet, y = noise)) +
+    geom_boxplot() +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90)) +
+    labs(x = "", y = "Signal-adjusted noise")
+    
+  })
+  
+  output$percent_138_plot <- renderPlot({
+    
+    ggplot(percent_138_results, aes(x = worksheet, 
+                                    y = percent_whole_panel_covered_at_138x)) +
+      geom_boxplot() +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 90)) +
+      labs(x = "", y = "Percentage panel covered at 138x")
     
   })
   
