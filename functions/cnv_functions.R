b059e428975a5bc235f12943e81da06271f0aaf7
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-08-13 14:13:36 +0100

    Add count_primers_in_region function

--- functions/cnv_functions.R
+++ functions/cnv_functions.R
@@ -15,18 +15,20 @@ csv_timestamp <- function(table, folder) {
   #' Save a table as a comma-separated file with a timestamp
   #'
   #' @param table The table to save
-  #' @param folder The folder to save the file into
+  #' @param folder The filepath of the folder to save the file into, without a 
+  #' backlash at the end
   #'
-  #' @return Saves the table in the tables folder with a timestamp
+  #' @return Saves the table in the desired folder with a timestamp
   #'
   #' @note This function is used for exporting tables from RStudio for inclusion
   #' in validation documentation.
   #'
-  #' @examples csvt_timestamp(erbb2_table)
+  #' @examples csv_timestamp(gene_tbl_for_doc, paste0(outputs_folder, "tables"))
   
   write.csv(table,
             file = paste0(
               folder,
+              "/",
               format(Sys.time(), "%Y_%m_%d_%H_%M_%S"),
               "_",
               deparse(substitute(table)), ".csv"
@@ -769,6 +771,56 @@ grch38_primers <- read_csv(file = paste0(data_folder,
 
 grch38_primer_coordinates <- extract_cnv_coordinates(df = grch38_primers,
                                                      cnv_coord_col = region)
+
+count_primers_in_region <- function(chrom, coord1, coord2, df) {
+  #' Count the QIAseq primers within a region
+  #'
+  #' @param chrom The chromosome of the region of interest
+  #' @param coord1 The first coordinate of the region of interest
+  #' @param coord2 The second coordinate of the region of interest
+  #' @param df The dataframe containing the primer coordinate information.
+  #' This should be in the format of one primer per row with one column each for the 
+  #' primer start and end coordinates. 
+  #' 
+  #' @return Returns the number of primers within the specified region
+  #' 
+  #' @examples count_primers_in_region(chrom = "17", coord1 = 39700064,
+  #' coord2 = 39728658)
+  
+  if(typeof(chrom) != "character") {
+    stop("Chromosome argument must be a character")
+  }
+  
+  if (typeof(coord1) != "double" |
+      typeof(coord2) != "double") {
+    stop("Coordinates must be type double")
+  }
+  
+  if(!("primer_start" %in% colnames(df) & "primer_end" %in% colnames(df))) {
+    stop("Primer dataframe does not contain primer_start and primer_end columns")
+  }
+  
+  region_start <- min(coord1, coord2)
+  
+  region_end <- max(coord1, coord2)
+  
+  df2 <- df |> 
+    filter(chromosome == chrom) |> 
+    mutate(in_region = case_when(
+      
+      (primer_start >= region_start &
+         primer_start <= region_end) |
+        (primer_end >= region_start &
+           primer_end <= region_end) ~"Yes",
+      TRUE ~"No"
+      
+    ))
+  
+  primers_in_region <- sum(df2$in_region == "Yes")
+  
+  return(primers_in_region)
+  
+}
 
 # Genes and exons -------------------------------------------------------------------
 
