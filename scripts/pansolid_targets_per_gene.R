953f003830ff686092b42ad2b266ac3168d0acf0
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-08-15 13:38:11 +0100

    Add information on gene targets

--- scripts/pansolid_targets_per_gene.R
+++ scripts/pansolid_targets_per_gene.R
@@ -23,6 +23,10 @@ gene_tbl <- read_excel(path = paste0(data_folder, "transcripts/gene_labels.xlsx"
 
 amp_genes <- load_gene_table("Amplifications")
 
+target_df <- read_csv(paste0(data_folder,
+                             "bed_files/PanSolidv2_GRCh38_noalt_BED.csv")) |> 
+  clean_names()
+
 # Get primer coordinates ------------------------------------------------------------
 
 primer_df_mod <- extract_cnv_coordinates(df = primer_df, cnv_coord_col = region) |> 
@@ -43,10 +47,19 @@ gene_tbl_mod <- gene_tbl |>
            label == "EGFRvIII") |> 
   arrange(label)
 
+# Count targets for each gene -------------------------------------------------------
+
+gene_target_counts <- target_df |> 
+  filter(name %in% amp_genes$gene) |> 
+  count(name)
+
 gene_tbl_for_doc <- gene_tbl_mod |> 
   mutate(grch38_coordinates = str_c("chr", chromosome, ":", gene_start, "-", gene_end)) |> 
   rename(gene = label) |> 
   select(gene, grch38_coordinates, gene_size_kb, transcript_refseq,
-         primers_in_gene)
+         primers_in_gene) |> 
+  left_join(gene_target_counts, 
+            join_by("gene" == "name")) |> 
+  rename(targets_in_gene = n)
 
 csv_timestamp(gene_tbl_for_doc, paste0(outputs_folder, "tables"))
