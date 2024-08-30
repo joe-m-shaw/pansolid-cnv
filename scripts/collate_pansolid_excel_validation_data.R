067b86024c70e72ce50741e3325bbbfbc2a431d5
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-08-30 16:40:43 +0100

    Add scripts for collating validation data

---
+++ scripts/collate_pansolid_excel_validation_data.R
@@ -0,0 +1,104 @@
+# Load and Collate Data for PanSolid Gene Amplifications Cohort
+
+# Packages --------------------------------------------------------------------------
+
+library(tidyverse)
+library(here)
+
+# Functions -------------------------------------------------------------------------
+
+source(here("functions/pansolid_excel_functions.R"))
+
+source(here("scripts/set_shared_drive_filepath.R"))
+
+# Files -----------------------------------------------------------------------------
+
+amp_cohort_data_location <- "S:/central shared/Genetics/NGS/Bioinformatics/1_Pan-solid-Cancer/CNV/Amplifications_final_with_MYC/"
+
+amp_cohort_filepaths <- list.files(path = amp_cohort_data_location, 
+                               full.names = TRUE,
+                               recursive = TRUE,
+                               pattern = "Annotated_.+.xlsx")
+
+if(length(amp_cohort_filepaths) == 0) {
+  stop("No files in location")
+}
+
+# Collate results -------------------------------------------------------------------
+
+pos_cnv_results_collated <- amp_cohort_filepaths |> 
+  map(\(amp_cohort_filepaths) read_pos_cnv_results(file = amp_cohort_filepaths,
+                                                   sheet = get_amp_sheetname(
+                                                     amp_cohort_filepaths))) |> 
+  list_rbind()
+
+fold_change_threshold <- 2.8
+
+all_amp_gene_results_collated <- amp_cohort_filepaths |> 
+  map(\(amp_cohort_filepaths) read_all_amp_genes_results(file = amp_cohort_filepaths,
+                                                         sheet = get_amp_sheetname(
+                                                           amp_cohort_filepaths))) |> 
+  list_rbind() |> 
+  mutate(pansolid_call = case_when(
+    max_region_fold_change >= fold_change_threshold ~"amplification",
+    max_region_fold_change < fold_change_threshold ~"normal result"))
+
+stdev_results_collated <-  amp_cohort_filepaths |> 
+  map(\(amp_cohort_filepaths) read_stdev_results(file = amp_cohort_filepaths,
+                                                 sheet = get_amp_sheetname(
+                                                   amp_cohort_filepaths))) |> 
+  list_rbind() 
+
+percent_138_collated <- amp_cohort_filepaths |> 
+  map(\(amp_cohort_filepaths) 
+      read_percent_138_results(file = amp_cohort_filepaths,
+                               sheet = get_amp_sheetname(amp_cohort_filepaths))) |> 
+  list_rbind()
+
+
+# Check all results collated --------------------------------------------------------
+
+if(
+  
+  unique(unique(pos_cnv_results_collated$filepath) == amp_cohort_filepaths) != TRUE |
+  
+  unique(unique(all_amp_gene_results_collated$filepath) == amp_cohort_filepaths) != TRUE |
+  
+  unique(stdev_results_collated$filepath == amp_cohort_filepaths) != TRUE |
+  
+  unique(percent_138_collated$filepath == amp_cohort_filepaths) != TRUE
+  
+) {
+  
+  stop("Not all filepaths have been read in") 
+  
+} else {
+  
+  message("All PanSolid files successfully read in")
+  
+} 
+
+# Save collated results -------------------------------------------------------------
+
+collated_validation_data_folder <- paste0(data_folder, 
+                                          "validation/collated/")
+
+write.csv(x = pos_cnv_results_collated,
+          file = paste0(collated_validation_data_folder, 
+                        "validation_pos_cnv_results_collated.csv"),
+          row.names = FALSE)
+
+write.csv(x = all_amp_gene_results_collated,
+          file = paste0(collated_validation_data_folder, 
+                        "validation_all_amp_gene_results_collated.csv"),
+          row.names = FALSE)
+
+write.csv(x = stdev_results_collated,
+          file = paste0(collated_validation_data_folder, 
+                        "validation_stdev_results_collated.csv"),
+          row.names = FALSE)
+
+write.csv(x = percent_138_collated,
+          file = paste0(collated_validation_data_folder, 
+                        "validation_percent_138_collated.csv"),
+          row.names = FALSE)
---
+++ scripts/collate_wgs_htmls.R
@@ -0,0 +1,107 @@
+# Collate whole genome sequencing HTML data
+
+# Libraries and functions -----------------------------------------------------------
+
+library(here)
+library(tidyverse)
+library(readxl)
+
+source(here("scripts/set_shared_drive_filepath.R"))
+source(here("functions/wgs_html_functions.R"))
+
+# WGS HTML filepaths ----------------------------------------------------------------
+
+wgs_htmls <- list.files(path = paste0(data_folder, 
+                                      "validation/raw/wgs/"),
+                        full.names = TRUE,
+                        pattern = "*.html")
+
+# Read HTML CNV data ----------------------------------------------------------------
+
+wgs_domain1_cnvs <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_table_by_div_id(
+    html_filepath = wgs_htmls, 
+    div_id = "d_svcnv_tier1")) |> 
+  list_rbind() |> 
+  mutate(domain = 1)
+
+wgs_domain2_cnvs <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_table_by_div_id(
+    html_filepath = wgs_htmls, 
+    div_id = "d_svcnv_tier2")) |> 
+  list_rbind() |> 
+  mutate(domain = 2)
+
+wgs_domain3_cnvs <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_table_by_div_id(
+    html_filepath = wgs_htmls, 
+    div_id = "d_svcnv_tier3")) |> 
+  list_rbind() |> 
+  mutate(domain = 3)
+
+# Collate WGS CNV data --------------------------------------------------------------
+
+wgs_html_cnvs <- rbind(wgs_domain1_cnvs  |> 
+                    select(-c(population_germline_allele_frequency,
+                              gene_mode_of_action)), 
+                  wgs_domain2_cnvs |> 
+                    select(-c(population_germline_allele_frequency,
+                           gene_mode_of_action)), wgs_domain3_cnvs) |> 
+  mutate(cnv_class = parse_wgs_cnv_class(col = variant_type),
+         cnv_copy_number = parse_wgs_cnv_copy_number(col = variant_type),
+         chromosome = parse_wgs_html_grch38_coordinates(col = variant_gr_ch38_coordinates,
+                                                        group = "chromosome"),
+         cnv_start = as.numeric(parse_wgs_html_grch38_coordinates(col = variant_gr_ch38_coordinates,
+                                                                  group = "first coordinate")),
+         cnv_end = as.numeric(parse_wgs_html_grch38_coordinates(col = variant_gr_ch38_coordinates,
+                                                                group = "second coordinate")),
+         category = "WGS result")
+
+# Read HTML identifiers -------------------------------------------------------------
+
+wgs_pids <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_pid_text(wgs_htmls)) |> 
+  list_rbind()
+
+wgs_headers <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_header(wgs_htmls)) |> 
+  list_rbind()
+
+wgs_tumour_details <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_table_by_div_id(
+    html_filepath = wgs_htmls, 
+    div_id = "t_tumour_details")) |> 
+  list_rbind() |> 
+  rename(wgs_pathno = histopathology_or_sihmds_lab_id) |> 
+  select(filepath, wgs_pathno)
+
+# Get lab numbers -------------------------------------------------------------------
+
+wgs_pathway_tracker <- read_excel(path = paste0(data_folder, 
+                                                "WGS pathway tracker_copy_2024-07-01.xlsx"),
+                                  sheet = "Cancer") |> 
+  janitor::clean_names()
+
+wgs_pathway_tracker_dna_no_df <- wgs_pathway_tracker |> 
+  filter(!is.na(mol_db_number) & 
+           !is.na(ngis_referral_id) &
+           sample_type == "Solid tumour" &
+           !duplicated(ngis_referral_id)) |> 
+  mutate(labno = str_extract(string = mol_db_number,
+                             pattern = "\\d{8}")) |> 
+  select(labno, ngis_referral_id)
+
+# Collate identifiers ---------------------------------------------------------------
+
+wgs_html_ids <- inner_join(x = wgs_headers, y = wgs_pids, by = "filepath") |> 
+  left_join(wgs_pathway_tracker_dna_no_df, join_by("wgs_r_no" == "ngis_referral_id"),
+            relationship = "one-to-one") |> 
+  left_join(wgs_tumour_details, by = "filepath")
+
+# Export collated information -------------------------------------------------------
+
+write_csv(x = wgs_html_ids,
+          file = paste0(data_folder, "validation/collated/wgs_html_ids.csv"))
+
+write_csv(x = wgs_html_cnvs,
+          file = paste0(data_folder, "validation/collated/wgs_html_cnvs.csv"))
