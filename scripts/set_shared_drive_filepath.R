d96ef41fe81650c73742273c8f295a5fb9c37f71
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-08-30 16:41:33 +0100

    Add script for setting shared drive filepath

---
+++ scripts/set_shared_drive_filepath.R
@@ -0,0 +1,7 @@
+
+# Modify this filepath if necessary
+shared_drive_path <- "S:/central shared/Genetics/Mol_Shared/Development.Team/Pan Solid CLC Somatic Amplifications Validation/pansolid_cnv_r_files/"
+
+data_folder <- paste0(shared_drive_path, "data/")
+
+outputs_folder <- paste0(shared_drive_path, "outputs/")
