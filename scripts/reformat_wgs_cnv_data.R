813e3ae15c8ed65a8407f1d5562165e1101b3566
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-09-04 14:59:08 +0100

    Update script to work with new function file

--- scripts/reformat_wgs_cnv_data.R
+++ scripts/reformat_wgs_cnv_data.R
@@ -1,15 +1,16 @@
 # Reformat PanSolid and WGS data ----------------------------------------------------
 
 library(here)
-source(here("functions/cnv_functions.R"))
+library(tidyverse)
+source(here("functions/reformatting_functions.R"))
 source(here("scripts/set_shared_drive_filepath.R"))
 
 # Load collated data ----------------------------------------------------------------
 
-wgs_data_collated <- read_csv(file = paste0(data_folder, "validation/collated/",
+wgs_data_collated <- read_csv(file = paste0(data_folder, "validation/processed/",
                                                  "wgs_html_cnvs.csv"))
 
-wgs_html_ids <- read_csv(file = paste0(data_folder, "validation/collated/",
+wgs_html_ids <- read_csv(file = paste0(data_folder, "validation/processed/",
                                        "wgs_html_ids.csv"),
                          col_types = list(
                            "filepath" = col_character(),
@@ -31,24 +32,23 @@ wgs_htmls <- list.files(path = paste0(data_folder, "validation/raw/wgs/"),
                         full.names = TRUE,
                         pattern = "*.html")
 
-wgs_del_data_reformatted <- wgs_htmls |> 
-  map(\(wgs_htmls) reformat_wgs_cnv_result(filepath = wgs_htmls,
-                                           cnv_type = "Deletions")) |> 
-  list_rbind()
-
 wgs_amp_data_reformatted <- wgs_htmls |> 
   map(\(wgs_htmls) reformat_wgs_cnv_result(filepath = wgs_htmls,
-                                           cnv_type = "Amplifications")) |> 
+                                           cnv_type = "Amplifications",
+                                           wgs_tbl = wgs_data_collated)) |> 
   list_rbind()
-
-wgs_data_reformatted_collated <- rbind(wgs_del_data_reformatted, 
-                                       wgs_amp_data_reformatted)
 
 # Add identifiers -------------------------------------------------------------------
 
-wgs_reformatted_with_ids <- wgs_html_ids |> 
-  left_join(wgs_data_reformatted_collated, by = "filepath",
+wgs_amp_data_reformatted_with_ids <- wgs_html_ids |> 
+  left_join(wgs_amp_data_reformatted, by = "filepath",
             relationship = "one-to-many") |> 
   mutate(nhs_no_clean = as.character(nhs_no_clean),
          labno = as.character(labno))
-  
+
+# Export data -----------------------------------------------------------------------
+
+write.csv(wgs_amp_data_reformatted_with_ids,
+          file = paste0(data_folder, "validation/processed/",
+                        "wgs_amp_data_reformatted_with_ids.csv"),
+          row.names = FALSE)
