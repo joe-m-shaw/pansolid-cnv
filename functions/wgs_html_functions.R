4a18695247755cdaa24ede132e66f9dee926d60b
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-09-10 10:24:58 +0100

    Put regex in separate functions

--- functions/wgs_html_functions.R
+++ functions/wgs_html_functions.R
@@ -109,8 +109,7 @@ parse_wgs_html_pid_text <- function(html_filepath) {
 parse_wgs_html_table_by_div_id <- function(html_filepath,
                                            div_id) {
   
-  #' Parse whole genome sequencing HTML files using a CSS
-  #' "div_id" identifier.
+  #' Parse whole genome sequencing HTML files using a CSS "div_id" identifier.
   #'
   #' @param html_filepath The filepath for the WGS HTML file
   #' @param div_id The CSS identifier for the table (examples below)
@@ -132,6 +131,10 @@ parse_wgs_html_table_by_div_id <- function(html_filepath,
     rvest::html_table() |> 
     janitor::clean_names() |> 
     dplyr::mutate(filepath = html_filepath)
+  
+  if(nrow(output_table) == 0) {
+    warning("No rows in output table")
+  }
   
   return(output_table)
   
@@ -161,70 +164,123 @@ parse_wgs_html_table_by_number <- function(html_filepath,
     rvest::html_table() |> 
     janitor::clean_names()
   
+  if(nrow(output_table) == 0) {
+    warning("No rows in output table")
+  }
+  
   return(output_table)
   
 }
 
-wgs_html_variant_type_regex <- stringr::regex(
-  r"[
-  (GAIN|LOSS|LOH|INV|DUP|DEL|  # Standard variant types
-  .+)                          # Catch-all category
-  (\(\d{1,3}\)                 # Dosage number
-  |)                           # Value if absent
-  ]",
-  comments = TRUE
-)
 
-parse_wgs_cnv_class <- function(col) {
+wgs_html_variant_type_regex <- function() {
   
-  wgs_cnv_class <- stringr::str_extract(string = col,
-                               pattern = wgs_html_variant_type_regex,
+  #' Regular expression for whole genome sequencing "variant type" HTML column
+  #'
+  #' @return The regular expression for the "variant type" column (example format: LOH(2); GAIN(3)).
+  #' This output is then used for later functions for extracting different parts of the regex.
+  #' @export
+  
+  wgs_html_variant_type_regex <- stringr::regex(
+    
+    r"[
+    (GAIN|LOSS|LOH|INV|DUP|DEL|  # Standard variant types
+    .+)                          # Catch-all category
+    (\(\d{1,3}\)                 # Dosage number
+    |)                           # Value if absent
+    ]",
+    comments = TRUE
+    
+  )
+  
+  return(wgs_html_variant_type_regex)
+  
+}
+
+parse_wgs_cnv_class <- function(x) {
+  
+  #' Parse the CNV class from the WGS variant type string
+  #'
+  #' @param x The WGS variant type string from a WGS HTML which has the variant type and then may 
+  #' also have the copy number in brackets. Examples: "LOH(2)", "GAIN(3)", "INV"
+  #'
+  #' @return The CNV class (examples: "LOH", "GAIN", "INV") as a string
+  #' @export
+  #'
+  #' @examples cnv_class <- parse_wgs_cnv_class("LOH (2)")
+  
+  wgs_cnv_class <- stringr::str_extract(string = x,
+                               pattern = wgs_html_variant_type_regex(),
                                group = 1)
-  
-  if(anyNA(wgs_cnv_class, recursive = TRUE)) {
-    stop("NA values present")
-  }
   
   return(wgs_cnv_class)
   
 }
 
-parse_wgs_cnv_copy_number <- function(col) {
+
+parse_wgs_cnv_copy_number <- function(x) {
   
-  wgs_cnv_copy_number <- readr::parse_number(stringr::str_extract(string = {{ col }} ,
-                                                  pattern = wgs_html_variant_type_regex,
+  #' Parse the CNV copy number from the WGS variant type string
+  #'
+  #' @param x The WGS variant type string from a WGS HTML which has the variant type and then may 
+  #' also have the copy number in brackets. Examples: "LOH(2)", "GAIN(3)", "INV"
+  #'
+  #' @return The CNV copy number as a number
+  #' @export
+  #'
+  #' @examples cnv_number <- parse_wgs_cnv_copy_number("LOH (2)")
+  
+  wgs_cnv_copy_number <- readr::parse_number(stringr::str_extract(string = x,
+                                                  pattern = wgs_html_variant_type_regex(),
                                                   group = 2))
   
   return(wgs_cnv_copy_number)
   
 }
 
-wgs_html_grch38_coordinates_regex <- stringr::regex(
-  r"[
-  (\d{1,2}|X|Y)        # Chromosome
-  :
-  (\d{1,10})           # First coordinate
-  (-|:)
-  (\d{1,10})           # Second coordinate
-  ]",
-  comments = TRUE
-)
-
-parse_wgs_html_grch38_coordinates <- function(col, group) {
+wgs_html_grch38_coordinates_regex <- function() {
   
-  if(!group %in% c("chromosome", "first coordinate", "second coordinate")) {
-    stop("group must be either chromosome, first coordinate or second coordinate")
-  }
+  #' Regular expression for the whole genome sequencing variant coordinates string
+  #'
+  #' @return The regular expression for the "Variant GRCh38 coordinates" column in the WGS HTML file.
+  #' The format gives the chromosome, start coordinate and end coordinate for a CNV.
+  #' Example: "7:60912080-159335569"
+  #' @export
   
-  group_choice <- dplyr::case_when(
+  wgs_html_grch38_coordinates_regex <- stringr::regex(
     
-    group == "chromosome" ~1,
-    group == "first coordinate" ~2,
-    group == "second coordinate" ~4)
+      r"[
+      (\d{1,2}|X|Y)        # Chromosome
+      :
+      (\d{1,10})           # First coordinate
+      (-|:)
+      (\d{1,10})           # Second coordinate
+      ]",
+      comments = TRUE
+      
+    )
   
-  output <- stringr::str_extract(string  = col,
-                        pattern = wgs_html_grch38_coordinates_regex,
-                        group = group_choice)
+  return(wgs_html_grch38_coordinates_regex)
+  
+}
+
+parse_wgs_html_grch38_coordinates <- function(x, group) {
+  
+  #' Parse a group from the whole genome sequencing GRCh38 coordinate string
+  #'
+  #' @param x The coordinate string. Example: "7:60912080-159335569"
+  #' @param group The group to be selected, which must be 1 for "chromosome", 2 for "first coordinate"
+  #' or 4 for "second coordinate".
+  #'
+  #' @return The selected regex group as a string.
+  #' @export
+  #'
+  #' @examples cnv_chromosome <- parse_wgs_html_grch38_coordinates(x = "7:60912080-159335569",
+  #' group = 1)
+  
+  output <- stringr::str_extract(string = x,
+                        pattern = wgs_html_grch38_coordinates_regex(),
+                        group = group)
   
   return(output)
   
