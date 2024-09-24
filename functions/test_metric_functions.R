ea323f036aa57607a98b4307cedbedf71a87d57e
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-09-24 10:20:30 +0100

    Add specificity calculations with 95% CI

--- functions/test_metric_functions.R
+++ functions/test_metric_functions.R
@@ -39,3 +39,35 @@ calc_specificity <- function(tp, tn, fp, fn) {
   
 }
 
+
+get_epir_metrics <- function(outcome_vector) {
+  
+  #' Get test metrics using epiR
+  #'
+  #' @param outcome_vector Numeric vector in the order: true positives, false positives,
+  #' false negatives, true negatives
+  #'
+  #' @return A dataframe with test metrics formatted for publication.
+  #' @export
+  #'
+  #' @examples
+  
+  epi_test <- epiR::epi.tests(dat = outcome_vector,
+                   digits = 2, conf.level = 0.95, method = "exact")
+
+  epi_test_df <- tibble::tibble(epi_test$detail) |> 
+    dplyr::filter(statistic %in% c("se", "sp", "pv.pos", "pv.neg")) |> 
+    dplyr::mutate(
+      `Percentage (%)` = round(est*100, 1),
+      `Confidence interval (95%)` = str_c(round(lower*100, 1), "-", round(upper*100, 1)),
+      Metric = case_when(
+        statistic == "se" ~"Sensitivity",
+        statistic == "sp" ~"Specificity",
+        statistic == "pv.pos" ~"Positive predictive value",
+        statistic == "pv.neg" ~"Negative predictive value"
+      )) |> 
+    dplyr::select(Metric, `Percentage (%)`, `Confidence interval (95%)`)
+  
+  return(epi_test_df)
+  
+}
