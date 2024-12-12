ef22bcd46dc95c81d62cf9dab2aaf82e61d375e4
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2024-12-12 10:45:06 +0000

    Add in tier 2 and 2 CNVs from WGS

--- scripts/del_val_collate_wgs_htmls.R
+++ scripts/del_val_collate_wgs_htmls.R
@@ -19,10 +19,37 @@ wgs_del_htmls <- list.files(path = paste0(data_folder, "validation/",
 
 wgs_htmls <- c(wgs_del_htmls, wgs_amp_htmls)
 
-wgs_html_cnvs <- wgs_htmls |> 
-  map(\(wgs_htmls) parse_wgs_html_table_by_div_id(wgs_htmls, 
-                                                  "d_svcnv_tier1")) |> 
+# Read HTML CNV data ----------------------------------------------------------------
+
+wgs_domain1_cnvs <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_table_by_div_id(
+    html_filepath = wgs_htmls, 
+    div_id = "d_svcnv_tier1")) |> 
   list_rbind() |> 
+  mutate(domain = 1)
+
+wgs_domain2_cnvs <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_table_by_div_id(
+    html_filepath = wgs_htmls, 
+    div_id = "d_svcnv_tier2")) |> 
+  list_rbind() |> 
+  mutate(domain = 2)
+
+wgs_domain3_cnvs <- wgs_htmls |> 
+  map(\(wgs_htmls) parse_wgs_html_table_by_div_id(
+    html_filepath = wgs_htmls, 
+    div_id = "d_svcnv_tier3")) |> 
+  list_rbind() |> 
+  mutate(domain = 3)
+
+# Collate WGS CNV data --------------------------------------------------------------
+
+wgs_html_cnvs <- rbind(wgs_domain1_cnvs  |> 
+                         select(-c(population_germline_allele_frequency,
+                                   gene_mode_of_action)), 
+                       wgs_domain2_cnvs |> 
+                         select(-c(population_germline_allele_frequency,
+                                   gene_mode_of_action)), wgs_domain3_cnvs) |> 
   mutate(cnv_class = parse_wgs_cnv_class(x = variant_type),
          
          cnv_copy_number = parse_wgs_cnv_copy_number(x = variant_type),
@@ -35,8 +62,13 @@ wgs_html_cnvs <- wgs_htmls |>
          
          cnv_end = as.numeric(parse_wgs_html_grch38_coordinates(x = variant_gr_ch38_coordinates,
                                                                 group = 4)),
+         
+         # In the HTMLS,  clinical indication specific genes are annotated with "*".
+         # Remove this asterisk for easier filtering later on.
          gene = str_replace_all(string = gene, pattern = "\\*",
-                                replacement = ""))
+                                replacement = ""),
+         
+         category = "WGS result")
 
 # Read HTML identifiers -------------------------------------------------------------
 
