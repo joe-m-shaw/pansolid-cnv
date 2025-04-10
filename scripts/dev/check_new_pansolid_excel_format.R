3d97cdf047c2b1eaef80e261970d32640dc736e5
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-04-10 15:55:00 +0100

    Check functions work with old format

--- scripts/dev/check_new_pansolid_excel_format.R
+++ scripts/dev/check_new_pansolid_excel_format.R
@@ -11,8 +11,15 @@ new_excel_files <- list.files(
   pattern = "Annotated.*.xlsx"
 )
 
+old_excel_files <- list.files(path = paste0(config::get("data_folderpath"),
+                                            "validation/DOC6567_deletions/raw/",
+                                            "pansolid_ngs/v2PANSOLID/"),
+                              full.names = TRUE,
+                              recursive = FALSE,
+                              pattern = "Annotated.*.xlsx")
+
 find_pred_ncc <- function(input_sheet, 
-                          pred_ncc_string = "Predicted NCC") {
+                          pred_ncc_string = "Predicted NCC (%)") {
   
   pred_ncc_start <- find_match(input_sheet, "a",
                                pred_ncc_string) + 1
@@ -32,8 +39,19 @@ find_pred_ncc <- function(input_sheet,
 new_sheet <- read_cnv_sheet(filepath = new_excel_files[1], 
                             sheet_regex = "CNVs")
 
-del_genes <- find_del_genes(new_sheet, num_genes = 34)
+old_sheet <- read_cnv_sheet(filepath = old_excel_files[1],
+                            sheet_regex = "CNVs")
 
-amp_genes <- find_amp_genes(new_sheet, num_genes = 13)
+del_genes_new <- find_del_genes(new_sheet, num_genes = 34)
+
+del_genes_old <- find_del_genes(old_sheet, num_genes = 37)
+
+amp_genes_new <- find_amp_genes(new_sheet, num_genes = 13)
+
+amp_genes_old <- find_amp_genes(old_sheet, num_genes = 9)
 
 new_ncc <- find_pred_ncc(new_sheet)
+
+sig_cnvs_new <- find_sig_cnvs(new_sheet)
+
+sig_cnvs_old <- find_sig_cnvs(old_sheet)
