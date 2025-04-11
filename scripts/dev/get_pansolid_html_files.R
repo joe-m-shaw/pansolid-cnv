e3eb33cdf3efda07e46154e4fef42668fb7c6a52
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-04-11 14:17:00 +0100

    Add updates for new files

--- scripts/dev/get_pansolid_html_files.R
+++ scripts/dev/get_pansolid_html_files.R
@@ -11,12 +11,28 @@ data_folder <- config::get("data_folderpath")
 
 # S drive filepaths -----------------------------------------------------------------
 
-html_ws_df <- read_csv(paste0(data_folder, 
+pansolid_ws_df <- read_excel(path = paste0(data_folder, 
                               "live_service/",
-                              "pansolid_html_worksheets.csv"))
+                              "pansolid_html_worksheets.xlsx"),
+                         sheet = "pansolid_html_worksheets")
 
-# Modify this list with the worksheets you want to check
-worksheet_list <- html_ws_df$worksheet
+jbrca_ws_df <- read_excel(path = paste0(data_folder, 
+                                           "live_service/",
+                                           "pansolid_html_worksheets.xlsx"),
+                             sheet = "jbrca_html_worksheets")
+
+# New files to check ------------------------------------------------------
+
+pansolid_ws_to_check <- pansolid_ws_df |> 
+  filter(htmls_checked == "no")
+
+jbrca_ws_to_check <- jbrca_ws_df |> 
+  filter(htmls_checked == "no")
+
+ws_to_check_bind <- rbind(pansolid_ws_to_check,
+                          jbrca_ws_to_check)
+
+worksheet_list <- list(ws_to_check_bind$worksheet)
 
 get_html_filepaths <- function(
     repository_path = "S:/central shared/Genetics/Repository/WorksheetAnalysedData/",
@@ -43,4 +59,29 @@ s_drive_filepath_df <- tibble(
 
 file.copy(from = s_drive_filepath_df$path,
           to = paste0(data_folder, "live_service/htmls/"))
+
+# Files already checked ---------------------------------------------------
+
+pansolid_ws_checked <- pansolid_ws_df |> 
+  filter(htmls_checked == "yes")
+
+jbrca_ws_checked <- jbrca_ws_df |> 
+  filter(htmls_checked == "yes")
+
+ws_checked_bind <- rbind(pansolid_ws_checked,
+                         jbrca_ws_checked)
+
+checked_worksheet_list <- list(ws_checked_bind$worksheet)
+
+length(checked_worksheet_list[[1]])
+
+checked_filepaths <- checked_worksheet_list |> 
+  map(\(checked_worksheet_list) get_html_filepaths(worksheet = checked_worksheet_list)) |> 
+  flatten()
+
+length(checked_filepaths)
+
+# There are 16 samples so far with evidence of contamination
+
+(16 / length(checked_filepaths)) * 100
 
