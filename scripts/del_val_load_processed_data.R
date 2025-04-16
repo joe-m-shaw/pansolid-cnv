fe1095cf8f671d023f3474a1c8b55842320533c0
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-04-16 08:16:41 +0100

    Reorder and update with NCC data

--- scripts/del_val_load_processed_data.R
+++ scripts/del_val_load_processed_data.R
@@ -10,7 +10,7 @@ library(here)
 del_val_processed_folder <- paste0(config::get("data_folderpath"),
                                    "validation/DOC6567_deletions/processed/")
 
-# Load data --------------------------------------------------------------------
+# Patient information -----------------------------------------------------
 
 del_val_sample_patient_info <- read_csv(paste0(
   del_val_processed_folder, 
@@ -19,16 +19,30 @@ del_val_sample_patient_info <- read_csv(paste0(
                "labno" = col_character(),
                "firstname" = col_character(),
                "surname" = col_character(),
-               "date_in" = col_datetime(format = "%Y-%m-%d %H:%M:%OS"),
                "nhsno" = col_character(),
-               "pathno" = col_character(),
-               "comments" = col_character(),
-               "ncc" = col_character(),
+               "dob" = col_date(format = "%Y-%m-%d"),
+               "date_sample_received" = col_date(format = "%Y-%m-%d"),
+               "years_at_sample_receipt" = col_double(),
+               "tissue" = col_double(),
                "tissue_type" = col_character(),
-               "cancer_comment" = col_character(),
-               "cancer_group" = col_character(),
-               "method_name" = col_character()
-             ))
+               "pathno" = col_character(),
+               "ncc" = col_character(),
+               "extraction_method" = col_character(),
+               "extraction_batch_fk" = col_character(),
+               "concentration" = col_double(),
+               "comments" = col_character()))
+
+del_val_sample_cancer_types <- read_csv(paste0(
+  del_val_processed_folder,
+  "del_val_sample_cancer_types.csv"),
+  col_types = list(
+    "labno" = col_character(),
+    "comments" = col_character(),
+    "cancer" = col_character(),
+    "cancer_group" = col_character()
+  ))
+
+# Orthogonal test data ----------------------------------------------------
 
 del_val_ddpcr_collated <- read_csv(paste0(del_val_processed_folder, 
                                              "del_val_ddpcr_collated.csv"),
@@ -47,6 +61,8 @@ del_val_wgs_html_ids <- read_csv(paste0(del_val_processed_folder,
                            "labno" = col_character()
                          ))
 
+# PanSolid data -----------------------------------------------------------
+
 sample_id_col_list <- list("worksheet" = col_character(),
                            "labno" = col_character(),
                            "suffix" = col_character(),
@@ -55,11 +71,25 @@ sample_id_col_list <- list("worksheet" = col_character(),
                            "labno_suffix_worksheet" = col_character(),
                            "filepath" = col_character())
 
+del_val_collated_stdev <- read_csv(paste0(
+        del_val_processed_folder,
+        "del_val_collated_stdev.csv"),
+        col_types = c(sample_id_col_list,
+                      list(
+                        "stdev_noise" = col_double()
+                      )))
+
 del_val_collated_138x <- read_csv(paste0(del_val_processed_folder,
                                          "del_val_collated_138x.csv"),
                                   col_types = 
                                     c(sample_id_col_list, 
                                       list("percent_138x" = col_double())))
+
+del_val_collated_pred_ncc <- read_csv(paste0(del_val_processed_folder,
+                                             "del_val_collated_pred_ncc.csv"),
+                                      col_types = c(
+                                        sample_id_col_list,
+                                        list("pred_ncc" = col_double())))
 
 del_val_collated_amp_genes <- read_csv(paste0(
   del_val_processed_folder,
@@ -90,7 +120,9 @@ del_val_collated_loh <- read_csv(paste0(
                   "gene" = col_character(),
                   "ploidy_state" = col_character(),
                   "loh_status"  = col_character(),
-                  "no_targets_in_ploidy_region" = col_character()
+                  "no_targets_in_ploidy_region" = col_character(),
+                  "check_1" = col_character(),
+                  "check_2" = col_logical()
               )))
 
 del_val_collated_sig_cnvs <- read_csv(paste0(
@@ -112,24 +144,6 @@ del_val_collated_sig_cnvs <- read_csv(paste0(
                   "start" = col_double(),
                   "end" = col_double()
                 )))
-
-del_val_collated_stdev <- read_csv(paste0(
-  del_val_processed_folder,
-  "del_val_collated_stdev.csv"),
-  col_types = c(sample_id_col_list,
-                list(
-                  "stdev_noise" = col_double()
-                )))
-
-del_val_sample_cancer_types <- read_csv(paste0(
-  del_val_processed_folder,
-  "del_val_sample_cancer_types.csv"),
-  col_types = list(
-    "labno" = col_character(),
-    "comments" = col_character(),
-    "cancer" = col_character(),
-    "cancer_group" = col_character()
-  ))
 
 del_val_pansolid_worksheet_details <- read_csv(paste0(
   del_val_processed_folder,
