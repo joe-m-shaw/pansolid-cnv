a3c8d4f887f75c885409a3228a12ac0069880a79
Joe Shaw <57156322+joeshaw824@users.noreply.github.com>
2025-05-07 10:46:41 +0100

    Add monitoring script for deletions service

--- scripts/dev/monitor_pansolid_cnv_service.R
+++ scripts/dev/monitor_pansolid_cnv_service.R
@@ -16,7 +16,7 @@ all_worksheets <- dna_db_worksheets |>
 
 ps_ws_info <- all_worksheets |> 
   # New CNV Excel layout started with WS152758
-  filter(pcrid == 152758) |> 
+  filter(pcrid >= 152758) |> 
   filter(grepl(pattern = "pansolid", 
                x = description,
                ignore.case = TRUE)) |> 
@@ -46,10 +46,12 @@ ps_filepaths <- ps_worksheets |>
 
 stopifnot(length(ps_filepaths) != 0)
 
+filename_regex <- "Annotated_.*_WS\\d{6}_\\d{8}.*.xlsx"
+
 ps_filepath_df <- tibble(
   filepath = unlist(ps_filepaths)) |> 
   mutate(filename = str_extract(string = filepath,
-                                pattern = "Annotated_.*_WS\\d{6}_\\d{8}.*.xlsx"))
+                                pattern = filename_regex))
 
 stopifnot(anyNA.data.frame(ps_filepath_df) == FALSE)
 
@@ -76,77 +78,158 @@ amp_genes_live <- read_csv(paste0(collated_data_folder,
 del_genes_live <- read_csv(paste0(collated_data_folder,
                                   "del_genes_live.csv"))
 
+loh_live <- read_csv(paste0(collated_data_folder,
+                                  "loh_live.csv"))
+
 # Identify files not already collated -------------------------------------
 
-
-ps_filepath_df |> 
+ps_new_filepath_df <- ps_filepath_df |> 
   filter(!filename %in% stdev_live$filename)
 
+message(paste0(length(ps_new_filepath_df$filepath),
+               " new files identified"))
 
-# Stop if there are already files in the raw folder
+# Prepare raw data folder -------------------------------------------------
 
+raw_folder_path <- paste0(config::get("data_folderpath"),
+                          "live_service/raw/")
 
-# Check whether the filenames have already been collated
+if(length(list.files(raw_folder_path)) != 0){
+  stop("Raw file folder is not empty")
+}
 
-# Copy any new results into a local folder
+# Copy new files to raw data folder ---------------------------------------
 
-# Read in the results of those new files and collate them
+file.copy(from = ps_new_filepath_df$filepath,
+          to = raw_folder_path)
 
-# CNV tab, LOH, SVs
-
-# Perform checks to make sure the new data is the right format
-
-# Add those results to the collated data
-
-
-
-
-
-# Copy new files into raw folder ------------------------------------------
-
-file.copy(from = ps_filepath_df$filepath,
-          to = paste0(config::get("data_folderpath"),
-                      "live_service/raw/"))
-
-new_filepaths <- list.files(path = paste0(config::get("data_folderpath"),
-                                          "live_service/raw/"),
+new_filepaths <- list.files(path = raw_folder_path,
                             full.names = TRUE,
                             pattern = "Annotated.*.xlsx")
 
+message(paste0(length(new_filepaths), " files copied into raw data folder"))
 
-time_start <- Sys.time()
+# Collate data from new files ---------------------------------------------
 
-new_data <- new_filepaths |> 
+cnv_new <- new_filepaths |> 
   map(\(new_filepaths) extract_cnv_tbls(new_filepaths, 
-                                         sheet_regex = "CNVs_"))
-time_end <- Sys.time()
+                                        sheet_regex = "CNVs_"))
 
-message(paste0(length(new_filepaths), " CNV results collated"))
-
-stdev_df <- map(new_data, ~ .x[["stdev"]]) |> 
-  list_rbind()
-
-percent_138x_df <- map(new_data, ~ .x[["percent_138x"]]) |> 
-  list_rbind()
-
-pred_ncc_df <-  map(new_data, ~ .x[["pred_ncc"]]) |> 
-  list_rbind()
-
-sig_cnvs_df <- map(new_data, ~ .x[["sig_cnvs"]]) |> 
-  list_rbind()
-
-amp_genes_df <- map(new_data, ~ .x[["amp_genes"]]) |> 
-  list_rbind()
-
-del_genes_df <-  map(new_data, ~ .x[["del_genes"]]) |> 
-  list_rbind()
-
-
-loh <- new_filepaths |> 
+loh_new <- new_filepaths |> 
   map(\(new_filepaths) read_loh_table(filepath = new_filepaths)) |> 
   list_rbind() 
 
+message(paste0(length(new_filepaths), " CNV and LOH results collated"))
 
+stdev_new <- map(cnv_new, ~ .x[["stdev"]]) |> 
+  list_rbind() |>
+  mutate(filename = str_extract(string = filepath,
+                                pattern = filename_regex)) |> 
+  relocate(filename, .after = filepath)
+
+percent_138x_new <- map(cnv_new, ~ .x[["percent_138x"]]) |> 
+  list_rbind()|>
+  mutate(filename = str_extract(string = filepath,
+                                pattern = filename_regex)) |> 
+  relocate(filename, .after = filepath)
+
+pred_ncc_new <-  map(cnv_new, ~ .x[["pred_ncc"]]) |> 
+  list_rbind()|>
+  mutate(filename = str_extract(string = filepath,
+                                pattern = filename_regex)) |> 
+  relocate(filename, .after = filepath)
+
+sig_cnvs_new <- map(cnv_new, ~ .x[["sig_cnvs"]]) |> 
+  list_rbind()|>
+  mutate(filename = str_extract(string = filepath,
+                                pattern = filename_regex)) |> 
+  relocate(filename, .after = filepath)
+
+amp_genes_new <- map(cnv_new, ~ .x[["amp_genes"]]) |> 
+  list_rbind()|>
+  mutate(filename = str_extract(string = filepath,
+                                pattern = filename_regex)) |> 
+  relocate(filename, .after = filepath)
+
+del_genes_new <-  map(cnv_new, ~ .x[["del_genes"]]) |> 
+  list_rbind()|>
+  mutate(filename = str_extract(string = filepath,
+                                pattern = filename_regex)) |> 
+  relocate(filename, .after = filepath)
+
+# Perform checks ----------------------------------------------------------
+
+# Signal-adjusted noise
+
+stopifnot(anyNA.data.frame(stdev_new) == FALSE)
+
+stopifnot(min(stdev_new$stdev_noise) >= 0)
+
+# Percentage coverage at 138X
+
+stopifnot(anyNA.data.frame(percent_138x_new) == FALSE)
+
+stopifnot(min(percent_138x_new$percent_138x) >= 0)
+
+stopifnot(max(percent_138x_new$percent_138x) <= 100)
+
+# Predicted NCC
+
+stopifnot(anyNA.data.frame(pred_ncc_new) == FALSE)
+
+stopifnot(min(pred_ncc_new$pred_ncc) >= 20)
+
+stopifnot(max(pred_ncc_new$pred_ncc) <= 100)
+
+# Significant CNVs
+
+stopifnot(anyNA.data.frame(sig_cnvs_new |> 
+                             select(-c(check_1,	check_2,	
+                                    copy_number, copy_number,
+                                    start, end))) == FALSE)
+
+# Amplification genes
+
+stopifnot(anyNA.data.frame(amp_genes_new) == FALSE)
+
+# Deletion genes
+
+stopifnot(anyNA.data.frame(del_genes_new) == FALSE)
+
+# LOH
+
+stopifnot(anyNA.data.frame(loh_new |> 
+                             select(-c(check_1, check_2))) == FALSE)
+
+# Check all tables have been read from each file
+
+stopifnot(length(setdiff(stdev_new$filepath, percent_138x_new$filepath)) == 0)
+
+stopifnot(length(setdiff(stdev_new$filepath, pred_ncc_new$filepath)) == 0)
+
+stopifnot(length(setdiff(stdev_new$filepath, unique(sig_cnvs_new$filepath))) == 0)
+
+stopifnot(length(setdiff(stdev_new$filepath, unique(amp_genes_new$filepath))) == 0)
+
+stopifnot(length(setdiff(stdev_new$filepath, unique(del_genes_new$filepath))) == 0)
+
+stopifnot(length(setdiff(stdev_new$filepath, unique(loh_new$filepath))) == 0)
+
+# Add new results to existing data ----------------------------------------
+
+stdev_df <- rbind(stdev_live, stdev_new)
+
+percent_138x_df <- rbind(percent_138x_live, percent_138x_new)
+
+pred_ncc_df <- rbind(pred_ncc_live, pred_ncc_new)
+
+sig_cnvs_df <- rbind(sig_cnvs_live, sig_cnvs_new)
+
+amp_genes_df <- rbind(amp_genes_live, amp_genes_new)
+
+del_genes_df <- rbind(del_genes_live, del_genes_new)
+
+loh_df <- rbind(loh_live, loh_new)
 
 # Export collated data ----------------------------------------------------
 
@@ -169,7 +252,7 @@ df_list <- list(
   "sig_cnvs" = sig_cnvs_df,
   "amp_genes" = amp_genes_df,
   "del_genes" = del_genes_df,
-  "loh" = loh
+  "loh" = loh_df
 )
 
 imap(df_list, export_collated_data)
@@ -177,13 +260,13 @@ imap(df_list, export_collated_data)
 # Delete copied files -----------------------------------------------------
 
 message("Deleting new raw files")
+
 file.remove(new_filepaths)
 
 if(length(list.files(paste0(config::get("data_folderpath"),
                             "live_service/raw/"))) == 0){
   message("Raw file folder is empty")
 }
-
 
 # Clear environment -------------------------------------------------------
 
